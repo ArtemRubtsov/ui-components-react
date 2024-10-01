d8984d916f2dd361d0d0be0a91d6e6928f2ad699
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-01 22:53:21 +0300

    add style

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import styles from './card.module.scss'
+
 export const divType = ['string', 'number', 'boolean'] as const
 export type TypeDiv = (typeof divType)[number]
 
@@ -6,10 +9,14 @@ type Props = {
   customType: TypeDiv
 } & React.HTMLAttributes<HTMLDivElement>
 
-export const Card = ({ className, ...props }: Props) => {
+export const Card = ({ className, customType, ...props }: Props) => {
+  const cardClass = `${styles.card} ${className || ''}`
+
   return (
-    <>
-      <div className={className} {...props} />
-    </>
+    <div className={cardClass} {...props}>
+      {customType === 'string' && <p>This is a string type</p>}
+      {customType === 'number' && <p>This is a number type</p>}
+      {customType === 'boolean' && <p>This is a boolean type</p>}
+    </div>
   )
 }
