13308ccb46b65579fbf25fb9ebe2d84ab3d76ef5
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-05 23:16:17 +0300

    style: class complete, progress, review

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -1,14 +1,39 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Badge } from './badge'
+import { Badge, BadgeVariant } from './badge' // Убедитесь, что вы импортируете BadgeVariant
 
 const meta: Meta<typeof Badge> = {
+  argTypes: {
+    variant: {
+      control: { options: BadgeVariant, type: 'select' }, // Используем 'select' с вариантами
+    },
+  },
   component: Badge,
-}
+  tags: ['autodocs'],
+  title: 'Components/Badge',
+} satisfies Meta<typeof Badge>
 
 export default meta
+
 type Story = StoryObj<typeof Badge>
 
 export const Complete: Story = {
-  args: {},
+  args: {
+    title: 'Complete',
+    variant: 'Complete',
+  },
+}
+
+export const Progress: Story = {
+  args: {
+    title: 'In Progress',
+    variant: 'Progress',
+  },
+}
+
+export const Review: Story = {
+  args: {
+    title: 'Under Review',
+    variant: 'Review',
+  },
 }
--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,3 +1,29 @@
-export const Badge = () => {
-  return <div>badge</div>
+import { ElementType } from 'react'
+
+import style from './badge.module.scss'
+
+export const BadgeVariant = ['Complete', 'Progress', 'Review'] as const
+export type VariantBadge = (typeof BadgeVariant)[number]
+
+type Props<T extends ElementType = 'span'> = {
+  as?: T
+  title: string
+  variant?: VariantBadge
+}
+
+export const Badge = <T extends ElementType = 'span'>({
+  as,
+  title,
+  variant,
+  ...rest
+}: Props<T>) => {
+  const Component = as || 'span'
+
+  const className = variant ? `${style[`badge-${variant.toLowerCase()}`]}` : ''
+
+  return (
+    <Component className={className} {...rest}>
+      {title}
+    </Component>
+  )
 }
