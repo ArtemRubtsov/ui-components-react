b0d0850c2acc834f2037ddf5009aa4db24230d24
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-08 20:44:39 +0300

    Updates

--- assets/avatar.stories-70a4236b.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-d9bc4622.js";import{c as i}from"./clsx-1229b3e0.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const u="_avatar_z6ibh_1",p="_circle_z6ibh_10",s={avatar:u,default:"_default_z6ibh_6",circle:p},e=({alt:t,type:n,...d})=>r("div",{className:s.avatar,children:r("img",{alt:t||"avatar",className:i(s[n]),...d})});try{e.displayName="Avatar",e.__docgenInfo={description:"",displayName:"Avatar",props:{alt:{defaultValue:null,description:"",name:"alt",required:!1,type:{name:"string"}},borderRadius:{defaultValue:null,description:"",name:"borderRadius",required:!0,type:{name:"string"}},type:{defaultValue:null,description:"",name:"type",required:!0,type:{name:"enum",value:[{value:'"circle"'},{value:'"default"'}]}}}}}catch{}const v={argTypes:{type:{control:{type:"radio"},options:["default","circle"]}},component:e,tags:["autodocs"],title:"Components/Avatar"},a={args:{alt:"default",borderRadius:"3px",src:"",type:"default"}};var l,o,c;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
-  args: {
-    alt: 'default',
-    borderRadius: '3px',
-    src: '',
-    type: 'default'
-  }
-}`,...(c=(o=a.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const g=["Default"];export{a as Default,g as __namedExportsOrder,v as default};
--- assets/syntaxhighlighter-NMPM6SWI-36f57191.js
+++
@@ -1,1 +0,0 @@
-import{S as c,c as l,s as n}from"./DocsRenderer-NNNQARDV-abb5c3ae.js";import"./iframe-83bbd370.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-c468d7b4.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
