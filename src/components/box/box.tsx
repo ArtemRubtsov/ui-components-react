84ebb13b5ad99ef78c029b482fa5435c0263f3ae
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-14 00:34:37 +0300

    feat: add Box type and clx display

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { ElementType } from 'react'
 
-export const Box = () => {
-  return <div>box</div>
+import clsx from 'clsx'
+
+import s from './box.module.scss'
+
+type BoxProps<T extends ElementType = 'div' | 'span'> = {
+  as?: T
+  children?: React.ReactNode
+  className?: string
+  display?: 'block' | 'inline' | 'inline-block'
+}
+
+export const Box = <T extends ElementType = 'div'>(props: BoxProps<T>) => {
+  const { as: Component = 'div', children, className, display = 'block', ...rest } = props
+
+  return (
+    <Component className={clsx(s[display])} {...rest}>
+      {children}
+    </Component>
+  )
 }
