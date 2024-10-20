54dfb3f3f84ed274e335e38280675858085aad12
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-20 20:14:29 +0300

    refactor: css property. change type ImgHTMLAttributes<HTMLImageElement>, added className

--- src/components/avatar/avatar.stories.tsx
+++ src/components/avatar/avatar.stories.tsx
@@ -4,6 +4,16 @@ import { Avatar, Variant } from './'
 
 const meta = {
   argTypes: {
+    align: {
+      control: { type: 'select' },
+      description: 'Size of the avatar',
+      options: ['justify-center', 'justify-end', 'justify-start', 'space-around', 'space-between'],
+    },
+    justify: {
+      control: { type: 'select' },
+      description: 'Size of the avatar',
+      options: ['justify-center', 'justify-end', 'justify-start', 'space-around', 'space-between'],
+    },
     src: {
       control: 'text',
       description: 'URL of  the image',
@@ -28,7 +38,9 @@ type story = StoryObj<typeof meta>
 
 export const Default: story = {
   args: {
+    align: 'align-stretch',
     alt: 'default',
+    justify: 'justify-center',
     size: { height: '24px', width: '24px' },
     src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ACm_TQdcAeevcSrggAAGAlW86M2-HgNzAw&s',
     type: 'default',
@@ -38,7 +50,9 @@ export const Default: story = {
 
 export const Circle: story = {
   args: {
+    align: 'align-stretch',
     alt: 'default',
+    justify: 'justify-center',
     size: { height: '24px', width: '24px' },
     src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ACm_TQdcAeevcSrggAAGAlW86M2-HgNzAw&s',
     type: 'circle',
