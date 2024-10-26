500f677d90da965b7316430664cc21540e41421c
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-26 20:48:47 +0300

    reafctor: props, styles and stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -4,10 +4,14 @@ import { Button } from './'
 
 const meta = {
   argTypes: {
+    circle: {
+      control: { type: 'check' },
+      options: ['circle'],
+    },
     onClick: { action: 'clicked' },
     variant: {
       control: { type: 'radio' },
-      options: ['primary', 'secondary', 'tertiary', 'link'],
+      options: ['primary', 'secondary', 'tertiary', 'link', 'default'],
     },
   },
   component: Button,
@@ -18,11 +22,68 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+export const ButtonStory: Story = {
+  render: () => {
+    return (
+      <>
+        <div
+          style={{ display: 'flex', flexDirection: 'column', gap: '15px', marginBottom: '20px' }}
+        >
+          <Button variant={'default'}>Default Button</Button>
+          <Button variant={'primary'}>Primary Button</Button>
+          <Button variant={'secondary'}>Secondary Button</Button>
+          <Button variant={'link'}>Link</Button>
+          <Button variant={'link2'}>Link2</Button>
+        </div>
+        <hr />
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', maxWidth: '200px' }}>
+            <h2 style={{ fontFamily: 'monospace', fontWeight: 'normal' }}>
+              Without circle property
+            </h2>
+            <Button variant={'default'}>Default Button</Button>
+            <Button variant={'primary'}>Primary Button</Button>
+            <Button variant={'secondary'}>Secondary Button</Button>
+            <Button variant={'link'}>Link</Button>
+            <Button variant={'link2'}>Link2</Button>
+          </div>
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', maxWidth: '200px' }}>
+            <h2 style={{ fontFamily: 'monospace', fontWeight: 'normal' }}>With circle property</h2>
+            <Button circle variant={'default'}>
+              Default Button
+            </Button>
+            <Button circle variant={'primary'}>
+              Primary Button
+            </Button>
+            <Button circle variant={'secondary'}>
+              Secondary Button
+            </Button>
+            <Button circle variant={'link'}>
+              Link
+            </Button>
+            <Button circle variant={'link2'}>
+              Link2
+            </Button>
+          </div>
+        </div>
+      </>
+    )
+  },
+}
+
 export const Primary: Story = {
   args: {
     children: 'Primary Button',
     disabled: false,
     variant: 'primary',
+  },
+}
+
+export const Default: Story = {
+  args: {
+    children: 'Default Button',
+    disabled: false,
+    variant: 'default',
   },
 }
 
@@ -33,18 +94,20 @@ export const Secondary: Story = {
     variant: 'secondary',
   },
 }
-export const Tertiary: Story = {
-  args: {
-    children: 'Tertiary Button',
-    disabled: false,
-    variant: 'tertiary',
-  },
-}
+
 export const Link: Story = {
   args: {
     children: 'Button that looks like a link',
     disabled: false,
     variant: 'link',
+  },
+}
+
+export const Link2: Story = {
+  args: {
+    children: 'Button that looks like a link',
+    disabled: false,
+    variant: 'link2',
   },
 }
 
