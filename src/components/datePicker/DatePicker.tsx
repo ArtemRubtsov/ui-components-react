da7fc08332d03b18f046256cf1b50406ae371a47
Artem Rubtsov <ua.rubtsov.a@gmail.com>
2024-12-23 22:21:37 +0200

    feat: DatePicker

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -9,6 +9,7 @@ type DatePickerProps = {}
 export default function DatePicker() {
   const [selectedDate, setSelectedDate] = useState<null | string>(null)
   const [isOpen, setIsOpen] = useState(false)
+  const [selectTime, setSelectTime] = useState<null | string>(null)
 
   const today = new Date()
   const currentYear = today.getFullYear()
@@ -20,12 +21,27 @@ export default function DatePicker() {
     return Array.from({ length: daysInMonth }, (_, i) => i + 1)
   }
 
+  const generateTimeSlots = () => {
+    const timeSlots = []
+
+    for (let hour = 9; hour <= 19; hour++) {
+      timeSlots.push(`${String(hour).padStart(2, '0')}:00`)
+      timeSlots.push(`${String(hour).padStart(2, '0')}:30`)
+    }
+
+    return timeSlots
+  }
+
   const handleDateClick = (day: number) => {
     const formattedDate = `${currentYear}-${String(currentMonth + 1).padStart(2, '0')}-${String(
       day
     ).padStart(2, '0')}`
 
     setSelectedDate(formattedDate)
+    setIsOpen(false)
+  }
+  const handleTimeClick = (time: string) => {
+    setSelectTime(time)
     setIsOpen(false)
   }
 
@@ -37,15 +53,24 @@ export default function DatePicker() {
         onClick={() => setIsOpen(true)}
         readOnly
         type={'text'}
-        value={selectedDate || ''}
+        value={`${selectedDate || ''} ${selectTime || ''}`.trim()}
       />
       {isOpen && (
-        <div className={s.calendar}>
-          {generateDays(currentYear, currentMonth).map(day => (
-            <button key={day} onClick={() => handleDateClick(day)} type={'button'}>
-              {day}
-            </button>
-          ))}
+        <div className={s.wrapCalendar}>
+          <div className={s.calendar}>
+            {generateDays(currentYear, currentMonth).map(day => (
+              <button key={day} onClick={() => handleDateClick(day)} type={'button'}>
+                {day}
+              </button>
+            ))}
+          </div>
+          <div className={s.time}>
+            {generateTimeSlots().map(time => (
+              <button key={time} onClick={() => handleTimeClick(time)} type={'button'}>
+                {time}
+              </button>
+            ))}
+          </div>
         </div>
       )}
     </div>
