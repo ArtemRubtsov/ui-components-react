0a566e72efc8d8802135b73eb11f59407a337f4b
Artem Rubtsov <ua.rubtsov.a@gmail.com>
2025-03-06 18:23:06 +0200

    modal

---
+++ storybook-static/assets/text.stories-23242256.js
@@ -0,0 +1,76 @@
+import{j as e}from"./jsx-runtime-c64c8078.js";import{T as r}from"./text-a6e0b78e.js";import"./index-8365acb2.js";import"./clsx-1229b3e0.js";const p={component:r,title:"Components/Text"},s={parameters:{group:"ElementType"},render:n=>e.jsxs(e.Fragment,{children:[e.jsx(r,{as:"p",...n,children:"This is a paragraph element"}),e.jsx(r,{as:"span",...n,children:"This is a span element"}),e.jsx(r,{as:"div",...n,children:"This is a div element"}),e.jsx(r,{as:"label",...n,children:"This is a label element"})]})},a={render:n=>e.jsx(r,{align:"center",as:"p",weight:"regular",wrap:"balance",...n,children:"This is a p element"})},i={render:n=>e.jsx(r,{align:"center",as:"span",weight:"regular",wrap:"balance",...n,children:"This is a span element"})},t={render:n=>e.jsx(r,{align:"left",as:"div",weight:"medium",wrap:"balance",...n,children:"This is a div element"})},l={render:n=>e.jsx(r,{align:"right",as:"label",weight:"light",wrap:"balance",...n,children:"This is a label element"})},x={parameters:{group:"Size"},render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{align:"center",as:"div",size:"xxs",children:"xxs"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"xs",children:"xs"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"s",children:"s"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"m",children:"m"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"l",children:"l"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"xl",children:"xl"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"xxl",children:"xxl"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"xxxl",children:"xxl"}),e.jsx("hr",{}),e.jsx(r,{align:"center",as:"div",size:"x-xl",children:"x-xl"})]})};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    group: 'ElementType'
+  },
+  render: (args: Props) => <>\r
+      <Text as={'p'} {...args}>\r
+        This is a paragraph element\r
+      </Text>\r
+      <Text as={'span'} {...args}>\r
+        This is a span element\r
+      </Text>\r
+      <Text as={'div'} {...args}>\r
+        This is a div element\r
+      </Text>\r
+      <Text as={'label'} {...args}>\r
+        This is a label element\r
+      </Text>\r
+    </>
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: args => <Text align={'center'} as={'p'} weight={'regular'} wrap={'balance'} {...args}>\r
+      This is a p element\r
+    </Text>
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  render: args => <Text align={'center'} as={'span'} weight={'regular'} wrap={'balance'} {...args}>\r
+      This is a span element\r
+    </Text>
+}`,...i.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: args => <Text align={'left'} as={'div'} weight={'medium'} wrap={'balance'} {...args}>\r
+      This is a div element\r
+    </Text>
+}`,...t.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: args => <Text align={'right'} as={'label'} weight={'light'} wrap={'balance'} {...args}>\r
+      This is a label element\r
+    </Text>
+}`,...l.parameters?.docs?.source}}};x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    group: 'Size'
+  },
+  render: () => <>\r
+      <Text align={'center'} as={'div'} size={'xxs'}>\r
+        xxs\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'xs'}>\r
+        xs\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'s'}>\r
+        s\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'m'}>\r
+        m\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'l'}>\r
+        l\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'xl'}>\r
+        xl\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'xxl'}>\r
+        xxl\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'xxxl'}>\r
+        xxl\r
+      </Text>\r
+      <hr></hr>\r
+      <Text align={'center'} as={'div'} size={'x-xl'}>\r
+        x-xl\r
+      </Text>\r
+    </>
+}`,...x.parameters?.docs?.source}}};const o=["ElementVariants","ElementP","ElementSpan","ElementDiv","ElementLabel","Size"];export{t as ElementDiv,l as ElementLabel,a as ElementP,i as ElementSpan,s as ElementVariants,x as Size,o as __namedExportsOrder,p as default};
